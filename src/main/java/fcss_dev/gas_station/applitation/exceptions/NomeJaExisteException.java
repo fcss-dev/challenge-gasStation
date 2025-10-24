41a0de69ccab1ba2412f88e7d8011dd25bb40ebb
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-24 11:55:51 -0300

    controller repository metodo verificacao por nome

---
+++ src/main/java/fcss_dev/gas_station/applitation/exceptions/NomeJaExisteException.java
@@ -0,0 +1,7 @@
+package fcss_dev.gas_station.applitation.exceptions;
+
+public class NomeJaExisteException extends RuntimeException {
+    public NomeJaExisteException(String message) {
+        super(message);
+    }
+}

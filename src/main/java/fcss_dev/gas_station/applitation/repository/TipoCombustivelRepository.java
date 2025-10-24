472121442418d3622d9bcb793a90e080e58e9bcc
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-24 08:21:25 -0300

    service
    Tipo de combustivel,  CRUD

--- src/main/java/fcss_dev/gas_station/applitation/repository/TipoCombustivelRepository.java
+++ src/main/java/fcss_dev/gas_station/applitation/repository/TipoCombustivelRepository.java
@@ -1,9 +1,9 @@
 package fcss_dev.gas_station.applitation.repository;
 
+import fcss_dev.gas_station.applitation.model.TipoCombustivel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TipoCombustivel extends JpaRepository<TipoCombustivel, Long> {
-
+public interface TipoCombustivelRepository extends JpaRepository<TipoCombustivel, Long> {
 }
---
+++ src/main/java/fcss_dev/gas_station/applitation/service/TipoCombustivelService.java
@@ -0,0 +1,35 @@
+package fcss_dev.gas_station.applitation.service;
+
+import fcss_dev.gas_station.applitation.model.TipoCombustivel;
+import fcss_dev.gas_station.applitation.repository.TipoCombustivelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TipoCombustivelService {
+    @Autowired
+    private TipoCombustivelRepository repository;
+
+    // Create
+    public TipoCombustivel salvar(TipoCombustivel tipo){
+        return repository.save(tipo);
+    }
+    // Read
+    public List<TipoCombustivel> listarTodos(){
+        return repository.findAll();
+    }
+    public Optional<TipoCombustivel> listarPorId(Long id){
+        return repository.findById(id);
+    }
+
+    // update
+    public TipoCombustivel atualizar(TipoCombustivel tipo){
+        return repository.save(tipo);
+    }
+
+    // Delete
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}

b122412b2cacab88041e8bd6637820329d79fb07
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-24 11:56:56 -0300

    Revert "controller repository metodo verificacao por nome"
    This reverts commit 41a0de69ccab1ba2412f88e7d8011dd25bb40ebb.

--- src/main/java/fcss_dev/gas_station/applitation/exceptions/NomeJaExisteException.java
+++
@@ -1,7 +0,0 @@
-package fcss_dev.gas_station.applitation.exceptions;
-
-public class NomeJaExisteException extends RuntimeException {
-    public NomeJaExisteException(String message) {
-        super(message);
-    }
-}

dec219d2b7188ba34a41792bc8dd7b1927f01a1a
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-24 09:21:21 -0300

    tratamento de erros na Read

--- src/main/java/fcss_dev/gas_station/applitation/service/TipoCombustivelService.java
+++ src/main/java/fcss_dev/gas_station/applitation/service/TipoCombustivelService.java
@@ -1,6 +1,7 @@
 package fcss_dev.gas_station.applitation.service;
 
 import fcss_dev.gas_station.applitation.exceptions.DadosInvalidosException;
+import fcss_dev.gas_station.applitation.exceptions.NenhumRegistroEncontradoException;
 import fcss_dev.gas_station.applitation.model.TipoCombustivel;
 import fcss_dev.gas_station.applitation.repository.TipoCombustivelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +23,21 @@ public class TipoCombustivelService {
     }
     // Read
     public List<TipoCombustivel> listarTodos(){
-        return repository.findAll();
+        List<TipoCombustivel> tipos = repository.findAll();
+
+        if (tipos.isEmpty()) {
+            throw new NenhumRegistroEncontradoException("Nenhum tipo de combustível cadastrado");
+        }
+
+        return tipos;
     }
+
     public Optional<TipoCombustivel> listarPorId(Long id){
-        return repository.findById(id);
+        Optional<TipoCombustivel> tipo = repository.findById(id);
+        if (tipo.isEmpty()) {
+            throw new NenhumRegistroEncontradoException("Tipo de combustível com ID " + id + " não encontrado");
+        }
+        return tipo;
     }
 
     // update

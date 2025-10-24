8ac7eed37d2a2908c1de4e8d6cf2f374740d6322
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-24 11:58:42 -0300

    repository metodo verificacao por nome

--- src/main/java/fcss_dev/gas_station/applitation/repository/TipoCombustivelRepository.java
+++ src/main/java/fcss_dev/gas_station/applitation/repository/TipoCombustivelRepository.java
@@ -4,6 +4,10 @@ import fcss_dev.gas_station.applitation.model.TipoCombustivel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TipoCombustivelRepository extends JpaRepository<TipoCombustivel, Long> {
+    boolean existsByNome(String nome);
+    Optional<TipoCombustivel> findByNome(String nome);
 }

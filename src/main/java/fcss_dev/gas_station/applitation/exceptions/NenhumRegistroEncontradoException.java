10e3005e5dea97570f918e3a8aa015b700218574
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-24 09:20:30 -0300

    exception
    exception personalizada para Read

---
+++ src/main/java/fcss_dev/gas_station/applitation/exceptions/NenhumRegistroEncontradoException.java
@@ -0,0 +1,7 @@
+package fcss_dev.gas_station.applitation.exceptions;
+
+public class NenhumRegistroEncontradoException extends RuntimeException {
+    public NenhumRegistroEncontradoException(String message) {
+        super(message);
+    }
+}

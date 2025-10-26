18a671c0ebdd8c761637c1b4f4c49a775e9057eb
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-26 10:30:28 -0300

    método personalizado para buscar por nome

--- src/main/java/fcss_dev/gas_station/applitation/repository/BombaCombustivelRepository.java
+++ src/main/java/fcss_dev/gas_station/applitation/repository/BombaCombustivelRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BombaCombustivelRepository extends JpaRepository<BombaCombustivel, Long> {
+    boolean existsByNome(String nome);
 }

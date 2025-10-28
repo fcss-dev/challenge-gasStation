b0e9a4bfac103a0691cfa345733321d9d071f086
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-27 22:10:33 -0300

    READ SERVICE - ABASTECIMENTOS

--- src/main/java/fcss_dev/gas_station/applitation/service/AbastecimentoService.java
+++ src/main/java/fcss_dev/gas_station/applitation/service/AbastecimentoService.java
@@ -2,8 +2,11 @@ package fcss_dev.gas_station.applitation.service;
 
 import fcss_dev.gas_station.applitation.model.Abastecimento;
 import fcss_dev.gas_station.applitation.repository.AbastecimentoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -17,6 +20,13 @@ public class AbastecimentoService {
     }
 
     // READ SERVICE - ABASTECIMENTOS
+    public List<Abastecimento> listarTodosRegistros(){
+        return abastecimentoRepository.findAll();
+    }
+    public Abastecimento buscarRegistroPorId(Long id){
+        return abastecimentoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Abastecimento não encontrado com ID: " + id));
+    }
 
     // UPDATE SERVICE - ABASTECIMENTOS
 
